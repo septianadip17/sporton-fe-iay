46d3ecce9d120703cfb2d789659cc3cfbfd405a5
Septian Adi Pratama <septianadip7998@gmail.com>
2026-03-25 23:54:57 +0700

    about us edited

--- app/(landing)/about-us/page.tsx
+++ app/(landing)/about-us/page.tsx
@@ -3,6 +3,7 @@ const AboutUs = () => {
     <main>
       <h1 className="text-4xl font-bold text-primary">Halo ini About Us</h1>
       <p>ini semua tentang kami</p>
+      <p>belum bisa tampil ini</p>
     </main>
   );
 };
